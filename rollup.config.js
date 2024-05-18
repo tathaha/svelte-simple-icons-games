bed7771eeaca0e7bf0589d9c7bb866899a20e0b1
Chiraitori <77224364+tathaha@users.noreply.github.com>
2024-05-18 18:15:35 +0700

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -5,21 +5,21 @@ import resolve from 'rollup-plugin-node-resolve';
 import pkg from './package.json';
 
 const name = pkg.name
-	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
-	.replace(/^\w/, m => m.toUpperCase())
-	.replace(/-\w/g, m => m[1].toUpperCase());
+  .replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
+  .replace(/^\w/, m => m.toUpperCase())
+  .replace(/-\w/g, m => m[1].toUpperCase());
 
 export default {
-	input: 'src/index.js',
-	output: [
-		{ file: pkg.module, 'format': 'es' },
-		{ file: pkg.main, 'format': 'umd', name }
-	],
-	plugins: [
-		svelte({
-			customElement: true
-		}),
-		resolve(),
-		terser()
-	]
+  input: 'src/index.js',
+  output: [
+    { file: pkg.module, format: 'es' },
+    { file: pkg.main, format: 'umd', name }
+  ],
+  plugins: [
+    svelte({
+      customElement: true
+    }),
+    resolve(),
+    terser()
+  ]
 };
